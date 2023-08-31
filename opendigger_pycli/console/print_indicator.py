28454343e3ed751051de3bd0625981c01af7c7b4
chenjunjie <chenjunjie@megvii.com>
2023-08-31 14:13:02 +0800

    🦄 refactor: move print_failed_query to utils

--- opendigger_pycli/console/print_indicator.py
+++ opendigger_pycli/console/print_indicator.py
@@ -6,6 +6,7 @@ from . import CONSOLE
 from .print_indicator_graph import print_base_data_graph, print_base_network_data_graph
 from .print_indicator_json import print_base_data_json, print_base_network_data_json
 from .print_indicator_table import print_base_data_table, print_base_network_data_table
+from .utils import print_failed_query
 
 if t.TYPE_CHECKING:
     from opendigger_pycli.datatypes import (
@@ -22,29 +23,6 @@ SURPPORTED_DISPLAY_FORMAT_TYPE = t.Literal["table", "graph", "json"]
 
 def format_indicator_name(indicator_name: str) -> str:
     return indicator_name.replace("_", " ").title()
-
-
-def print_failed_query(
-    indicator_name: str, failed_query: t.Optional["IndicatorQuery"]
-) -> None:
-    if failed_query is None:
-        return
-
-    if failed_query.years:
-        CONSOLE.print(
-            f"[red]No {indicator_name} Indicator Data in years: "
-            f"{list(failed_query.years)}"
-        )
-    if failed_query.months:
-        CONSOLE.print(
-            f"[red]No {indicator_name} Indicator Data in months: "
-            f"{list(failed_query.months)}"
-        )
-    if failed_query.year_months:
-        CONSOLE.print(
-            f"[red]No {indicator_name} Indicator Data in year_months: "
-            f"{[f'{year_month[0]}-{year_month[1]:02}' for year_month in failed_query.year_months]}"
-        )
 
 
 def print_trivial_indicator(
--- opendigger_pycli/console/utils.py
+++ opendigger_pycli/console/utils.py
@@ -1,7 +1,10 @@
 import typing as t
 
+from . import CONSOLE
+
 if t.TYPE_CHECKING:
     from opendigger_pycli.datatypes import NameAndValue, NameNameAndValue
+    from opendigger_pycli.datatypes.query import IndicatorQuery
 
 
 def if_prettey(value: t.Any) -> bool:
@@ -13,3 +16,26 @@ def if_prettey(value: t.Any) -> bool:
             value = t.cast(t.List["NameNameAndValue"], value)
             return True
     return False
+
+
+def print_failed_query(
+    indicator_name: str, failed_query: t.Optional["IndicatorQuery"]
+) -> None:
+    if failed_query is None:
+        return
+
+    if failed_query.years:
+        CONSOLE.print(
+            f"[red]No {indicator_name} Indicator Data in years: "
+            f"{list(failed_query.years)}"
+        )
+    if failed_query.months:
+        CONSOLE.print(
+            f"[red]No {indicator_name} Indicator Data in months: "
+            f"{list(failed_query.months)}"
+        )
+    if failed_query.year_months:
+        CONSOLE.print(
+            f"[red]No {indicator_name} Indicator Data in year_months: "
+            f"{[f'{year_month[0]}-{year_month[1]:02}' for year_month in failed_query.year_months]}"
+        )
