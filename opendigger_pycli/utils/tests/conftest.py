f3931eed085143188060b42b570731df145eba07
chenjunjie <chenjunjie@megvii.com>
2023-09-30 22:26:53 +0800

    🧪 test: remove github api tests

--- opendigger_pycli/utils/tests/conftest.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-
-import pytest
-
-
-@pytest.fixture()
-def test_github_pat() -> str:
-    with open(Path(__file__).with_name("github_pat.txt")) as f:
-        return f.read()
